c6121dafd0540bdfaea7664496ebb86783d52c3c
Richard Mao <richard.mao@crowdnetic.com>
2019-04-17 16:18:20 -0400

    Example for Runnable lambda expression thread method: start(), sleep(), setName, setPriority, setUncaughtExceptionHandler(function)

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # javaBasic
 my java practice repo
+
+
+udemy/concurrency : https://www.udemy.com/java-multithreading-concurrency-performance-optimization/learn/v4/t/lecture/11198608?start=0
---
+++ src/com/richard/udemy/Main.java
@@ -0,0 +1,26 @@
+package com.richard.udemy;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+        //new Runnable() void run()
+        Thread  thread = new Thread(() -> {
+                System.out.println("We are in thread: " + Thread.currentThread().getName() + " running this thread.");
+                System.out.println("We are in thread: " + Thread.currentThread().getPriority() + " current priority.");
+            throw new RuntimeException("hahahhahahh hehehehehheh");
+
+        });
+        thread.setName("sb123");
+        thread.setPriority(Thread.MAX_PRIORITY);
+        //Thread.UncaughtExceptionHandler() void uncaughtException(Thread t, Throwable e)
+        thread.setUncaughtExceptionHandler((Thread t, Throwable e) -> {
+                System.out.println("In setUncaughtExceptionHandler, message is " + e.getMessage() + "Thread is " + t.getName());
+        });
+        //useful thread method.
+        System.out.println("We are in thread: " + Thread.currentThread().getName() + " before starting a new thread");
+        thread.start();
+        System.out.println("We are in thread: " + Thread.currentThread().getName() + " after starting a new thread");
+        Thread.sleep(500);
+
+    }
+}
